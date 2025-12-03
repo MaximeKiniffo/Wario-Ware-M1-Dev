64bc1c7066ae3ddc7c9678ac7f204d29c5819be6
Eva POTTIER <eva.pottier@etu.unicaen.fr>
2025-12-03 13:58:27 +0100

    add balloons

--- piqueballon/piqueballon_script.js
+++ piqueballon/piqueballon_script.js
@@ -5,16 +5,42 @@ let correctBalloon = null;
 // ----- INITIALISATION DU JEU -----
 function startGame() {
     // préparation du jeu
+    createBalloons();
 }
 
 // ----- CRÉATION DES BALLONS -----
 function createBalloons() {
     // génération des ballons
+    const container = document.querySelector(".balloons-area");
+    const maxX = container.offsetWidth - 60;  
+    const maxY = container.offsetHeight - 80;
+    for(var i = 0; i < 5 ; i++){
+        const balloon = document.createElement("div");
+        balloon.classList.add("balloon");
+
+        
+
+        balloon.style.left = Math.random() * maxX + "px";
+        balloon.style.top = Math.random() * maxY + "px";
+
+        balloon.addEventListener("click", handleBalloonClick);
+
+        container.appendChild(balloon);
+    }
+    const balloonToPop = document.createElement("div");
+    balloonToPop.classList.add("balloon-to-pop");
+
+    balloonToPop.style.left = Math.random() * maxX + "px";
+    balloonToPop.style.top = Math.random() * maxY + "px";
+
+    balloonToPop.addEventListener("click", handleBalloonClick);
+
+    container.appendChild(balloonToPop);    
 }
 
 // ----- GÉRER LE CLIC SUR UN BALLON -----
 function handleBalloonClick(event) {
-    // logique du clic
+    alert("Ballon cliqué !");
 }
 
 // ----- FIN DE PARTIE -----
@@ -25,4 +51,5 @@ function endGame(win) {
 // ----- LANCEMENT -----
 window.onload = () => {
     startGame();
+    
 };
