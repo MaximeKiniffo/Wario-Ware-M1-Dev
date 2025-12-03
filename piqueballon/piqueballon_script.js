b1413fa283bcd47066e185aaf8c67e90d38da0f0
Eva POTTIER <eva.pottier@etu.unicaen.fr>
2025-12-03 12:17:47 +0100

    begin containers

---
+++ piqueballon/piqueballon_script.js
@@ -0,0 +1,28 @@
+// ----- VARIABLES GLOBALES -----
+let balloons = [];
+let correctBalloon = null;
+
+// ----- INITIALISATION DU JEU -----
+function startGame() {
+    // préparation du jeu
+}
+
+// ----- CRÉATION DES BALLONS -----
+function createBalloons() {
+    // génération des ballons
+}
+
+// ----- GÉRER LE CLIC SUR UN BALLON -----
+function handleBalloonClick(event) {
+    // logique du clic
+}
+
+// ----- FIN DE PARTIE -----
+function endGame(win) {
+    // victoire ou défaite
+}
+
+// ----- LANCEMENT -----
+window.onload = () => {
+    startGame();
+};
