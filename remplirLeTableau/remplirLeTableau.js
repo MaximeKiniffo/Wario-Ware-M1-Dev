2837e330cc29a7b871a92ec248b807b35ddd831e
Kyldof <kyllian.ronne@laposte.net>
2025-12-04 15:33:52 +0100

    ajout du principe de base du jeu

---
+++ remplirLeTableau/remplirLeTableau.js
@@ -0,0 +1,28 @@
+const cubes = document.querySelectorAll("td")
+let nbcubes = 0
+let colorSelect = "green"
+let colorUnSelect = "red"
+let compteur = 0
+
+const checkCube = (cube) => {
+    const backgroundColor = cube.style.backgroundColor
+    if (backgroundColor == colorSelect) {
+        cube.style.backgroundColor = colorUnSelect
+        compteur -= 1
+    } else {
+        cube.style.backgroundColor = colorSelect
+        compteur += 1
+    }
+    if (compteur >= 9) {
+        console.log("win")
+    }
+}
+
+cubes.forEach((e) => {
+    e.addEventListener("click", (event) => {
+        checkCube(e)
+
+    })
+    nbcubes += 1
+})
+
