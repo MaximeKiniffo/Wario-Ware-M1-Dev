32eeed76506bd838764eeaa5bbfe0fa5c0df9e6a
Kyldof <kyllian.ronne@laposte.net>
2025-12-04 15:58:12 +0100

    finalisation

--- Jeu10/jeu10.js
+++ Jeu10/jeu10.js
@@ -1,5 +1,12 @@
+let lost = false;
+let win = false;
 const sonClick = new Audio("assets/musicNoel.mp3");
+const tictac = new Audio("assets/tictac.mp3")
+
 document.addEventListener("click", (event) => {
+    //jouer le son tictac
+    tictac.currentTime = 0;
+    tictac.play()
     // Vérifie si ce n'est pas le bouton
     if (event.target.id !== "monBouton" && event.target.id !== "lost") {
         sonClick.currentTime = 0; // remet au début si on clique vite
@@ -15,30 +22,33 @@ document.addEventListener("click", (event) => {
     }
 });
 
-
-let lost = false;
 function bougerBouton() {
-    const bouton = document.getElementById("monBouton");
-    const gameZone = document.getElementById("gameZone");
-    if (!lost) {
-        const largeur = gameZone.clientWidth - bouton.offsetWidth;
-        const hauteur = gameZone.clientHeight - bouton.offsetHeight;
+    if (!win) {
+        const bouton = document.getElementById("monBouton");
+        const gameZone = document.getElementById("gameZone");
+        if (!lost) {
+            const largeur = gameZone.clientWidth - bouton.offsetWidth;
+            const hauteur = gameZone.clientHeight - bouton.offsetHeight;
 
-        const nouvelleGauche = Math.floor(Math.random() * largeur);
-        const nouvelleHaut = Math.floor(Math.random() * hauteur);
+            const nouvelleGauche = Math.floor(Math.random() * largeur);
+            const nouvelleHaut = Math.floor(Math.random() * hauteur);
 
-        bouton.style.left = nouvelleGauche + "px";
-        bouton.style.top = nouvelleHaut + "px";
+            bouton.style.left = nouvelleGauche + "px";
+            bouton.style.top = nouvelleHaut + "px";
+        }
     }
+
 }
 
-function handlePressButton() {
+function handlePressWin() {
     console.log("win")
-    GameManager.onWin();
+    win = true
+    document.getElementById("win").style.visibility = "visible";
+    setTimeout(() => {
+        console.log("jeu suivant")
+        GameManager.onWin();
+    }, 1000)
 }
-
-// bouge toutes les X secondes (ici 0.5 seconde)
-setInterval(bougerBouton, 700);
 
 //gestion du compte à rebours
 function startCountdown() {
@@ -48,20 +58,28 @@ function startCountdown() {
     countdownEl.textContent = timeLeft;
 
     const timer = setInterval(() => {
-        timeLeft--;
-        countdownEl.textContent = timeLeft;
+        if (!win) {
+            timeLeft--;
+            countdownEl.textContent = timeLeft;
 
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            lost = true;
-            document.getElementById("lost").style.visibility = "visible";
-            setTimeout(() => {
-                GameManager.onLose();
+            if (timeLeft <= 0) {
+                clearInterval(timer);
+                lost = true;
+                document.getElementById("lost").style.visibility = "visible";
+                setTimeout(() => {
+                    GameManager.onLose();
 
-            }, 2000);
+                }, 2000);
+            }
         }
+
     }, 1000);
 }
 
+// bouge toutes les X secondes (ici 0.7 seconde)
+setInterval(bougerBouton, 700);
+
 // Démarre le compte à rebours dès le chargement
 window.onload = startCountdown;
+
+
